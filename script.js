a47efb1225a4615294eec8825da4986d57e64a54
codyborges <codyborgesdev@gmail.com>
2022-03-10 20:53:30 -0500

    final touches

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ darkBtn.addEventListener('click', dark)
 
 //Functions
 function dark(){
-document.querySelector('h1').innerHTML = "It's Demon Time"
-  document.querySelector('h1').style.color = 'red'
-  document.querySelector('body').style.backgroundImage = 'url("https://images.unsplash.com/photo-1523521384060-d1bdc9e477b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80")'
+document.querySelector('h1').innerHTML = "Dark Mode"
+  document.querySelector('h1').style.color = 'white'
+  document.querySelector('body').style.backgroundImage = 'url("https://images.unsplash.com/photo-1468276311594-df7cb65d8df6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")'
 }
